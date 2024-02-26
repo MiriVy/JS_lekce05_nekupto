3d23ebaaa49c83eb4d511c35ea716a6365afa231
Miroslava Vybiralova <miroslava.vybiralova@gmail.com>
2024-02-26 17:19:58 +0100

    pridano reseni nekup to, obsah

--- index.js
+++ index.js
@@ -17,13 +17,45 @@ const product3 = {
 };
 
 const product1Element = document.querySelector("#product1")
+const product2Element = document.querySelector("#product2")
+const product3Element = document.querySelector("#product3")
 product1Element.classList.add("border-primary")
 
-
+/*
 const product2ButtonElement = document.querySelector("#product2Button")
 product2ButtonElement.classList.remove("btn-primary")
 
 const product3TitleElement = document.querySelector("#product3Title")
 product3TitleElement.classList.toggle("text-center")
+*/
 
+const product1HTML = `
+  <img class="card-img-top" src=${product1.image} alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`
+const product2HTML = `
+  <img class="card-img-top" src="${product2.image}" alt="Slepičkabelka">
+  <div class="card-body">
+  <h2 class="card-title">${product2.name}</h2>
+  <p class="card-text">${product2.description}</p>
+  </div>
+  <button type="button" id="product2Button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`
 
+const product3HTML = `
+  <img class="card-img-top" src="${product3.image}" alt="Anatoplavky">
+  <div class="card-body">
+  <h2 id="product3Title" class="card-title">${product3.name}</h2>
+  <p class="card-text">${product3.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+  </div>
+`
+
+product1Element.innerHTML = product1HTML
+product2Element.innerHTML = product2HTML
+product3Element.innerHTML = product3HTML
